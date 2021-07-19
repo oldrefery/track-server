eb68e5846d453f3f122e44a402a42f50c64bcf60
oldrefery <35261486+oldrefery@users.noreply.github.com>
2021-07-19 08:38:00 +0300

    feat: Add authorization, middleware for checking JWT. Creating and saving user

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,25 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+
+const User = mongoose.model("User");
+
 const router = express.Router();
 
 router.post("/signup", (req, res) => {
-  console.log(req.body);
-  res.send("You made a post request");
+  const { email, password } = req.body;
+
+  const user = new User({ email, password });
+  user
+    .save()
+    .then(() => {
+      const token = jwt.sign({ userId: user._id }, "MY_SECRET_KEY");
+
+      res.send({ token });
+    })
+    .catch((err) => {
+      return res.status(422).send(err.message);
+    });
 });
 
 module.exports = router;
